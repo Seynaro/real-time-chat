416e6833603aab259a7683d023950a9905392e98
Seynaro <xamzatseynaro@gmail.com>
2021-05-18 15:41:33 +0300

    add message layout/ project completion

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,20 +1,19 @@
 import React, {useContext} from 'react';
-import {CHAT_ROUTE, LOGIN_ROUTE} from "../utils/consts";
+import {Route, Switch, Redirect} from 'react-router-dom'
 import {privateRoutes, publicRoutes} from "../routes";
-import {Route, Redirect, Switch} from "react-router-dom";
-import {Context} from "../index";
+import {CHAT_ROUTE, LOGIN_ROUTE} from "../utils/consts";
 import {useAuthState} from "react-firebase-hooks/auth";
+import {Context} from "../index";
 
 const AppRouter = () => {
-
     const {auth} = useContext(Context)
-    const {user} = useAuthState(auth)
+    const [user] = useAuthState(auth)
 
     return user ?
         (
             <Switch>
                 {privateRoutes.map(({path, Component}) =>
-                <Route key={path} path={path} component={Component} exact={true}/>
+                    <Route key={path} path={path} component={Component} exact={true}/>
                 )}
                 <Redirect to={CHAT_ROUTE}/>
             </Switch>
@@ -23,12 +22,11 @@ const AppRouter = () => {
         (
             <Switch>
                 {publicRoutes.map(({path, Component}) =>
-                    <Route path={path} component={Component} exact={true}/>
+                    <Route key={path} path={path} component={Component} exact={true}/>
                 )}
                 <Redirect to={LOGIN_ROUTE}/>
             </Switch>
         )
-        ;
 };
 
 export default AppRouter;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,33 @@
 import React, {useContext} from 'react';
-import {Box, Button, Container, Grid} from "@material-ui/core";
+import {Button, Container, Grid} from "@material-ui/core";
+import Box from "@material-ui/core/Box";
 import {Context} from "../index";
 import firebase from "firebase";
 
-const Login = () => {
 
+const Login = () => {
     const {auth} = useContext(Context)
 
     const login = async () => {
         const provider = new firebase.auth.GoogleAuthProvider()
         const {user} = await auth.signInWithPopup(provider)
-
+        console.log(user)
     }
 
     return (
         <Container>
-            <Grid container style={{height: window.innerHeight - 50}}
-                  alignItems={'center'}
-                  justify={'center'}
+            <Grid container
+                  style={{height: window.innerHeight - 50}}
+                  alignItems={"center"}
+                  justify={"center"}
             >
                 <Grid style={{width:400, background: 'lightgray'}}
                       container
-                      aliggnItems={'center'}
-                      direction={'column'}>
+                      alignItems={"center"}
+                      direction={"column"}
+                >
                     <Box p={5}>
-                        <Button onClick={login} variant={'outlined'}>Войти с помощью Google</Button>
+                        <Button onClick={login} variant={"outlined"}>Войти с помощью Google</Button>
                     </Box>
                 </Grid>
             </Grid>
